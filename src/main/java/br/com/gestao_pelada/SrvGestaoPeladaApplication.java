3bdd76673392314f4ca52828f8163cfa2632228d
Eduardo Sena <eduardojpsena@gmail.com>
2025-10-05 14:30:03 -0300

    chore: inicializa projeto Spring Boot

---
+++ src/main/java/br/com/gestao_pelada/SrvGestaoPeladaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestao_pelada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvGestaoPeladaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvGestaoPeladaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestao_pelada/SrvGestaoPeladaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestao_pelada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvGestaoPeladaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
